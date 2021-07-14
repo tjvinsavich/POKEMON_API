d60e7d3ff199fbb259b876b64847715f9659e842
TJ Vinsavich <tjvinsavich@gmail.com>
2021-07-14 11:50:18 -0500

    finished TODOs

--- PokemonAPI/Program.cs
+++ PokemonAPI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
 using System.Net.Http;
 
 namespace PokemonAPI
@@ -7,17 +10,39 @@ namespace PokemonAPI
     {       
         static void Main(string[] args)
         {
-           string pokeURL = "https://pokeapi.co/api/v2/pokemon/";
+           string pokeURL = "https://pokeapi.co/api/v2/pokemon?limit=9&offset=251";
 
-         
+
             //TODO create a new instance of HttpClient called client.
+            var client = new HttpClient();
             //TODO use your client instance to get a response from the poke URL.
+            var response = client.GetStringAsync(pokeURL).Result;
+
             //TODO go to https://json2csharp.com and convert your json reponse to classes. Create a new class file in visual studio and paste the classes you created on the website.
             //TODO create a variable that = JsonConvert.DeserializeObject<YourRootClassGoesHere>(yourStringResponseGoesHere);
+            var root = JsonConvert.DeserializeObject<Root>(response);
             //TODO print the results from your
 
+            foreach (var r in root.results)
+            {
+                Console.WriteLine(r.name + " => " + r.url);
+            }
+
             //TODO use the pokemon url above and change it to try and call your favorite pokemon.
+            pokeURL = "https://pokeapi.co/api/v2/pokemon/257/";
+            response = client.GetStringAsync(pokeURL).Result;
+
             //TODO Use select token to try and grab a couple values from your pokemon and display them.
+
+            var pokeName = JObject.Parse(response).GetValue("name").ToString();
+
+            var pokeMoniker = JObject.Parse(response).SelectToken("abilities[0].ability.name").ToString();
+
+            var pokeType1 = JObject.Parse(response).SelectToken("types[0].type.name").ToString();
+            var pokeType2 = JObject.Parse(response).SelectToken("types[1].type.name").ToString();
+
+            Console.WriteLine($"\n{pokeName.ToUpper()}: The {pokeMoniker.ToUpper()} Pokémon");
+            Console.WriteLine($"It is a {pokeType1.ToUpper()} and {pokeType2.ToUpper()} type.");
         }
     }
 }
---
+++ PokemonAPI/Root.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonAPI
+{
+    public class Result
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Root
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public List<Result> results { get; set; }
+    }
+}
